604d7c626d190fe1a91fe9b2db412362e7adc8d9
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-18 20:21:41 +0000

    Create Portfolio Added To Portfolio Component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,16 +9,17 @@ const Portfolio = () => {
  
   return (
    
-    <><div className='adnew'>
-      <Button style={{ background: "rgba(150, 150, 150, 0.2)" }}>... More</Button>
-      <Button style={{ background: "rgb(56, 97, 251)", color: "rgb(255, 255, 255)" }}>{<PlusCircleOutlined />}Add New</Button>
+    <><><div className='adnew'>
+      <Button style={{ borderRadius:'10px',background: "rgba(150, 150, 150, 0.2)", color: "black"}}>... More</Button>
+      <Button style={{ borderRadius:'10px', background: "rgb(56, 97, 251)", color: "rgb(255, 255, 255)" }}>{<PlusCircleOutlined />}Add New</Button>
     </div>
-    <div className='portempty'>
-    <div className='etitle'>
-         This portfolio is empty
-      </div>
-      <div className='esubtitle'>Add any coins to get started</div>
-      </div>
+      <div className='plus'>{<PlusCircleOutlined />}Create portfolio</div>
+      <div className='portempty'>
+        <div className='etitle'>
+          This portfolio is empty
+        </div>
+        <div className='esubtitle'>Add any coins to get started</div>
+      </div></>
       </>
     
   )
