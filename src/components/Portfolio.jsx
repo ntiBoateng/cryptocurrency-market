aa2b28d0aaa7a2b1ba546a33ea895370fa919035
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-18 19:42:21 +0000

    Added Some Divs To Give Information About Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,10 +8,19 @@ import {PlusCircleOutlined} from '@ant-design/icons'
 const Portfolio = () => {
  
   return (
-    <div className='adnew'>
-      <Button style={{background: "rgba(150, 150, 150, 0.2)"}}>... More</Button>
-      <Button style={{background:"rgb(56, 97, 251)", color: "rgb(255, 255, 255)" }}>{<PlusCircleOutlined/>}Add New</Button>
-    </div>  
+   
+    <><div className='adnew'>
+      <Button style={{ background: "rgba(150, 150, 150, 0.2)" }}>... More</Button>
+      <Button style={{ background: "rgb(56, 97, 251)", color: "rgb(255, 255, 255)" }}>{<PlusCircleOutlined />}Add New</Button>
+    </div>
+    <div className='portempty'>
+    <div className='etitle'>
+         This portfolio is empty
+      </div>
+      <div className='esubtitle'>Add any coins to get started</div>
+      </div>
+      </>
+    
   )
 }
 
