31f5916dd779b6a4f8aafee0edcdf9fc2a79da32
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-18 13:16:34 +0000

    Create Portfolio Link To Portfolio

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import millify from "millify"
 import {Typography, Row, Col, Statistic} from "antd"
+import {PlusCircleOutlined} from '@ant-design/icons'
 import {Link} from "react-router-dom"
 
 import {Cryptocurrencies} from "../components"
+
 
 //api call 
 import {useGetCryptosQuery} from "../services/cryptoApi"
@@ -16,6 +18,8 @@ const Homepage = () => {
 
 const {data, isFetching} = useGetCryptosQuery(10)
 
+
+
 const globalStats = data?.data?.stats
 
 //console.log(data)
@@ -25,6 +29,10 @@ if(isFetching) return <Loader />
   return (
     <>
     <Title level={2} className="heading">Cryptocurrency Market Global Stats</Title>
+
+   <div className='plus'>{<PlusCircleOutlined/>}<Link to="Portfolio" style={{color: "black"}}>Create Portfolio</Link></div> 
+    
+
     <Row>
       <Col span={12}>
         <Statistic title="Total Cryptocurrencies" value={millify(globalStats.total)} />
