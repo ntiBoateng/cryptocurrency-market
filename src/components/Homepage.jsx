b96fc9c9977ec71666d6cc712aced0bb8cbd657a
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-23 06:06:35 +0000

    Added Select Button For Currency

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,64 +1,95 @@
-import React from 'react'
-import millify from "millify"
-import {Typography, Row, Col, Statistic} from "antd"
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic, Select } from "antd";
 
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import {Cryptocurrencies} from "../components"
+import { Cryptocurrencies } from "../components";
 
+//api call
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
-//api call 
-import {useGetCryptosQuery} from "../services/cryptoApi"
+import Loader from "./Loader";
 
-import Loader from "./Loader"
-
-const {Title} = Typography;
+const { Title } = Typography;
 
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
 
-const {data, isFetching} = useGetCryptosQuery(10)
+  const { Option } = Select;
 
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
 
+  const globalStats = data?.data?.stats;
 
-const globalStats = data?.data?.stats
+  //console.log(data)
 
-//console.log(data)
-
-if(isFetching) return <Loader />
+  if (isFetching) return <Loader />;
 
   return (
     <>
-    <Title level={2} className="heading">Cryptocurrency Market Global Stats</Title>
+      <div className="select_currency">
+        <Select
+          defaultValue="USD"
+          onChange={handleChange}
+          
+        >
+          <Option value="USD">$ USD</Option>
+          <Option value="EUR">€ EUR</Option>
+          <Option value="GHS">₵ GHS</Option>
+        </Select>
+      </div>
+      <Title level={2} className="heading">
+        Cryptocurrency Market Global Stats
+      </Title>
 
-  
-    
+      <Row>
+        <Col span={12}>
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={millify(globalStats.total)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total 24hr Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
+        </Col>
+      </Row>
 
-    <Row>
-      <Col span={12}>
-        <Statistic title="Total Cryptocurrencies" value={millify(globalStats.total)} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Total Market cap" value={millify(globalStats.totalMarketCap)} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Total 24hr Volume" value={millify(globalStats.total24hVolume)} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
-      </Col>
-    </Row>
-
-<div className="home-heading-container">
-  <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
-  <Title level={4} className="show-more"> <Link to="/cryptocurrencies">Show more</Link> </Title>
-</div>
-<Cryptocurrencies simplified />
-
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={4} className="show-more">
+          {" "}
+          <Link to="/cryptocurrencies">Show more</Link>{" "}
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
