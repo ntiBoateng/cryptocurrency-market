57a158931bb71d71e1080e479b92c2be76cfd52b
Ramsonodoi <ramson.odoi@amalitech.org>
2022-03-23 07:04:03 +0000

    Added Watchlist To All Currencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,62 +1,79 @@
-import React, {useState, useEffect} from 'react'
-import millify from "millify"
-import {Link} from "react-router-dom"
-import {Card, Row, Col, Input, Typography} from "antd"
+import React, { useState, useEffect } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input, Typography } from "antd";
 
-import {useGetCryptosQuery} from "../services/cryptoApi"
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
-import Loader from "./Loader"
+import Loader from "./Loader";
+import { StarOutlined } from "@ant-design/icons";
 
-const {Title} = Typography;
+const { Title } = Typography;
 
 const Cryptocurrencies = ({ simplified }) => {
- 
-  const count = simplified ? 10 : 100
-const {data: cryptosList, isFetching} = useGetCryptosQuery(count)
- 
- const [cryptos, setCryptos] = useState([])
-//console.log(cryptos)
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
 
-const [searchTerm, setSearchTerm] = useState("")
+  const [cryptos, setCryptos] = useState([]);
+  //console.log(cryptos)
 
-useEffect(()=>{
+  const [searchTerm, setSearchTerm] = useState("");
 
-const filteredData = cryptosList?.data?.coins.filter((coin)=> coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
-setCryptos(filteredData)
-},[cryptosList, searchTerm])
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
 
-if(isFetching) return <Loader />
+  if (isFetching) return <Loader />;
   return (
     <>
-    {! simplified && (
-      <Title level={2} style={{textAlign:"center", color: "blue black"}}> List of Cryptocurrencies</Title>
+      {!simplified && (
+        <Title level={2} style={{ textAlign: "center", color: "blue black" }}>
+          {" "}
+          List of Cryptocurrencies
+        </Title>
+      )}
+      {!simplified && (
+        <div className="search-crypto" style={{ textAlign: "left" }}>
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
 
-    )}
-   {! simplified && (
-      <div className="search-crypto" style={{textAlign: "left"}} >
-      <Input placeholder='Search Cryptocurrency' onChange={(e)=>setSearchTerm(e.target.value)} />
-    </div>
-   )}
-
-    <Row gutter={[32,32]} className="crypto-card-container">
-     { cryptos?.map((currency)=>(
-        <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
-         <Link to= {`/crypto/${ currency.uuid } `}>
-         <Card 
-         title={`${currency.rank}. ${currency.name}`}
-         extra={<img src={currency.iconUrl} className="crypto-image" />}
-         hoverable
-         >
-           <p>Price: {millify(currency.price)}</p>
-           <p>Market Cap: {millify(currency.marketCap)}</p>
-           <p>Daily Change: {millify(currency.change)}%</p>
-             </Card>
-         </Link>
-        </Col>
-      ))}
-    </Row>
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col
+            xs={24}
+            sm={12}
+            lg={6}
+            className="crypto-card"
+            key={currency.uuid}
+          >
+             <p className="addto_mainwatchlist">
+                  <StarOutlined />
+                  Add to Main Watchlist
+                </p>
+            <Link to={`/crypto/${currency.uuid} `}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={<img src={currency.iconUrl} className="crypto-image" />}
+                hoverable
+              >
+               
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
     </>
-  )
-}
+  );
+};
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
